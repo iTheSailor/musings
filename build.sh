119c22aa4e0a07463480b696090451a9da1242bc
iTheSailor <112807438+iTheSailor@users.noreply.github.com>
2024-06-21 15:37:08 -0400

    deploy attempt 2

--- build.sh
+++ build.sh
@@ -1,37 +1,14 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
+set -o errexit  # Exit on error
 
-cd backend
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
 pip install -r requirements.txt
 
-# Convert static asset files
+# Move React build to Django static folder
+mv frontend/build/* backend/static/
+
+# Collect static files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
+# Apply database migrations
 python manage.py migrate
-
-# go to the frontend directory
-cd ../frontend
-
-# Modify this line as needed for your package manager (npm, yarn, etc.)
-npm install
-
-# Build the frontend
-
-npm run build
-
-npm install -g serve
-
-#create .env file for the frontend
-echo "REACT_APP_API_URL=http://localhost:8000" > .env.local
-echo "REACT_APP_API_URL=http://localhost:8000" > .env
-
-
-#run frontend 
-
-serve -s build
-
-cd ../backend
